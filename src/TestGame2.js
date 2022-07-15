a22f5be331e4106f1490dd8654727622796ae471
nomoto from Visual Studio <yukimasa_nomoto@csc1980.co.jp>
2022-07-15 15:48:55 +0900

    移行 OK

--- src/TestGame2.js
+++ src/TestGame2.js
@@ -9,62 +9,20 @@ function Square(props){
     )
 }
 
-function Board({xIsNext , squares , onXIsNext , onFinish , onSquare}){
-
-
-    const calculateWinner = (squares) => {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-          ];
-          for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-              return squares[a];
-            }
-          }
-          return null;       
-    }
-
-    const winner = calculateWinner(squares)
+function Board({squares , onClick}){
     
     const renderSquare = (i) => {
         return(
             <Square 
                 value={squares[i]}
-                onClick={() =>{
-                    const copySquares = Object.create(squares)
-
-                    if(calculateWinner(copySquares) || copySquares[i]){
-                        return;
-                    }
-
-                    copySquares[i] = xIsNext? '✕' : '◯'
-                    onSquare(copySquares)
-                    onXIsNext(!xIsNext)
-                }}
+                onClick={() => onClick(i)}
             />
         )
     }
 
-    let status = 'Next palayer: ' + (xIsNext? '✕' : '◯')
-    if(winner) {
-        status = "勝者：" + winner
-
-        onFinish()
-    }
 
     return(
         <div>
-            <div className="status">
-                {status}
-            </div>
             <div className="board-row">
                 {renderSquare(0)}
                 {renderSquare(1)}
@@ -88,28 +46,72 @@ function TestGame2(){
     const[xIsNext,setXIsNext] = React.useState(true)
     const[myHistory,setMyHistory]=React.useState([{squares:Array(9).fill(null)}])
 
-    const handleNext = (result) =>{
-        setXIsNext(result)
-    }
-
-    const handleFinish = () => {
-        console.log('finish')
-    }
-
-    const handleSquares = (his) => {
-        setMyHistory([his])
-    }
 
     const current = myHistory[myHistory.length-1]
+
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+          ];
+          for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+              return squares[a];
+            }
+          }
+          return null;       
+    }
+
+    const winner = calculateWinner(current.squares)
+
+    let status = 'Next palayer: ' + (xIsNext? '✕' : '◯')
+    if(winner) {
+        status = "勝者：" + winner
+    }
 
     return(
         <div className="game">
             <div className="gmae-board">
-            <Board xIsNext = {xIsNext} squares={current}
-                onXIsNext={handleNext} onSquare={handleSquares} onFinish={handleFinish}/>
+            <Board  squares={current.squares} 
+                onClick={i =>{
+                    //console.log('click')
+
+                    //console.log("current.squares")
+                    //console.log(current.squares)
+
+                    const copySquares = [...current.squares]
+                    //console.log("copySquares")
+                    //console.log(copySquares)
+
+                    if(calculateWinner(copySquares) || copySquares[i]){
+                        return;
+                    }
+
+                    copySquares[i] = xIsNext? '✕' : '◯'
+
+                    //console.log("myHistory")
+                    //console.log(myHistory)
+
+                    const copyHistory =
+                        myHistory.concat({squares:copySquares})
+                    
+                    //console.log("copyHistory")
+                    //console.log(copyHistory)
+                    setMyHistory(copyHistory)
+                    setXIsNext(!xIsNext)
+                    
+                }}
+            />
             </div>
             <div className="game-info">
-            <div>{/* status */}</div>
+            <div className="status">{status}</div>
             <div>{/* TODO */}</div>
             </div>
         </div>    
